9a67b4bced6e277c4422fa2dd3fb3815e22d5411
birdapi <birdapi@gmail.com>
2016-05-03 11:54:17 -0700

    Add examples for CNAME and DNAME

--- src/main/java/net/limbomedia/dns/Starter.java
+++ src/main/java/net/limbomedia/dns/Starter.java
@@ -154,6 +154,22 @@ public class Starter {
 			r.setLastChange(new Date());
 			r.setValue("2001:0db8:85a3:0000:0000:8a2e:0370:7334");
 			zone.getRecords().add(r);
+
+			r = new XRecord();
+			r.setId(UUID.randomUUID().toString());
+			r.setName("test");
+			r.setType(XType.CNAME);
+			r.setLastChange(new Date());
+			r.setValue("www.example.com.");
+			zone.getRecords().add(r);
+
+			r = new XRecord();
+			r.setId(UUID.randomUUID().toString());
+			r.setName("sub");
+			r.setType(XType.DNAME);
+			r.setLastChange(new Date());
+			r.setValue("example.org.");
+			zone.getRecords().add(r);
 			
 			result.add(zone);
 			
